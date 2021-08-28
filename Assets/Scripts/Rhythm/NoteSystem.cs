8de7e1926dd18739dec6e9137a5b8d63e845fc7d
Daniel Windhede <daniel.windhede@gmail.com>
2021-08-28 15:29:04 +0200

    Split rhythm input system
    Update Readme.asset
    Revert "Update Readme.asset"
    
    This reverts commit 6ad590023ab4ae3b250dcfb7245db318c4517f53.
    
    Revert "Revert "Update Readme.asset""
    
    This reverts commit 317479d356a22afaf069a62ae3a84f9c3b58a48c.

--- Assets/Scripts/Rhythm/NoteSystem.cs
+++ Assets/Scripts/Rhythm/NoteSystem.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Rhythm
 {
     public class NoteSystem : MonoBehaviour
     {
         [SerializeField] private AudioSource _audio;
-        [SerializeField] private GameObject item;
-        [SerializeField] private int startSegment = 50;
-        [SerializeField] private int endSegment = 1000;
-        [SerializeField] private Note.Direction direction;
+        [SerializeField] private GameObject exampleNote;
+        [SerializeField] private Text sampleText;
+        [SerializeField] private Canvas canvas;
+
+        [System.Serializable]
         class Note
         {
             public enum Direction
@@ -23,33 +25,46 @@ namespace Rhythm
                 Left = -1,
             }
 
-            public bool hasInteracted;
-            public int start;
-            public int end;
-            public Direction direction;
+            public enum NoteState
+            {
+                Idle,
+                Hit,
+                Miss,
+            }
 
-            public Note(int start, int end, Direction direction)
+            public Direction direction;
+            public int start;
+            [HideInInspector] public int end;
+
+            [HideInInspector] public NoteState state;
+            [HideInInspector] public GameObject gameObject;
+            public Note(int start, Direction direction)
             {
                 this.start = start;
-                this.end = end;
-                this.hasInteracted = false;
+                this.end = start + 10;
+                this.state = NoteState.Idle;
                 this.direction = direction;
             }
         }
 
         private Dictionary<Note.Direction, HashSet<Note>> notes = new Dictionary<Note.Direction, HashSet<Note>>()
-    {
-        { Note.Direction.Up,    new HashSet<Note>() },
-        { Note.Direction.Right, new HashSet<Note>() },
-        { Note.Direction.Down,  new HashSet<Note>() },
-        { Note.Direction.Left,  new HashSet<Note>() },
-    };
+        {
+            { Note.Direction.Up,    new HashSet<Note>() },
+            { Note.Direction.Right, new HashSet<Note>() },
+            { Note.Direction.Down,  new HashSet<Note>() },
+            { Note.Direction.Left,  new HashSet<Note>() },
+        };
+
+        [SerializeField]
+        private List<Note> noteCollection;
 
         void Start()
         {
-            AddNote(new Note(startSegment, endSegment, direction));
+            foreach (var note in noteCollection)
+            {
+                AddNote(new Note(note.start, note.direction));
+            }
             //AddNote(new Note(startSegment, _audio.clip.samples / 1024));
-            item.SetActive(false);
         }
 
         private void AddNote(Note note)
@@ -65,11 +80,9 @@ namespace Rhythm
             }
             if (_audio.isPlaying)
             {
-                var list = GetNotesAtSample(_audio.timeSamples / 1024, direction);
-                if (list != null)
-                {
-                    item.SetActive(true);
-                }
+                int sample = _audio.timeSamples / 1024;
+
+                InstantiateNotes(sample);
 
                 Vector2 controlDirection = Vector2.zero;
 
@@ -83,9 +96,12 @@ namespace Rhythm
                     controlDirection = Vector2.left;
 
                 if (controlDirection != Vector2.zero)
-                    MarkNotesAtSample(_audio.timeSamples / 1024, VectorToDirection(controlDirection));
+                {
+                    MarkNotesAtSample(sample, VectorToDirection(controlDirection));
+                }
 
-                RemoveNotes(_audio.timeSamples / 1024);
+                RemoveNotes(sample);
+                sampleText.text = "Sample: " + sample;
             }
         }
 
@@ -93,32 +109,99 @@ namespace Rhythm
         {
             return (Note.Direction)(direction.x + direction.y * 2);
         }
+        private Vector2 DirectionToVector(Note.Direction direction)
+        {
+            switch (direction)
+            {
+                case Note.Direction.Up:
+                    return Vector2.up;
+                case Note.Direction.Right:
+                    return Vector2.right;
+                case Note.Direction.Down:
+                    return Vector2.down;
+                case Note.Direction.Left:
+                    return Vector2.left;
+                default:
+                    return Vector2.zero;
+            }
+        }
+
+        private void InstantiateNotes(int sample)
+        {
+            foreach (Note.Direction dir in Enum.GetValues(typeof(Note.Direction)))
+            {
+                //var noteList = notes[dir].Where(x => x.start <= sample && x.end >= sample && x.state == Note.NoteState.Idle && !x.gameObject).ToList();
+                var noteList = notes[dir].Where(x => x.start <= sample && x.end >= sample && !x.gameObject).ToList();
+                foreach (var note in noteList)
+                {
+                    var obj = Instantiate(exampleNote, canvas.transform);
+                    obj.GetComponent<RectTransform>().localPosition = DirectionToVector(dir) * 150;
+                    if (note.state != Note.NoteState.Idle)
+                        obj.GetComponent<Image>().color = note.state == Note.NoteState.Hit ? Color.green : Color.red;
+                    note.gameObject = obj;
+                }
+            }
+        }
 
         private List<Note> GetNotesAtSample(int sample, Note.Direction direction)
         {
-            var list = notes[direction].Where(x => x.start <= sample && x.end >= sample && !x.hasInteracted).ToList();
+            var list = notes[direction].Where(x => x.start <= sample && x.end >= sample && x.state == Note.NoteState.Idle).ToList();
             if (list.Count > 0)
                 return list;
             return null;
         }
+        int sampleLeniency = 10;
         private void MarkNotesAtSample(int sample, Note.Direction direction)
         {
-            foreach (var note in notes[direction].Where(x => x.start <= sample && x.end >= sample).ToList())
+            var adjacentNotes = notes[direction].Where(x => x.start - sampleLeniency <= sample && x.end + sampleLeniency >= sample).ToList();
+            var hitableNotes = adjacentNotes.Where(x => x.start <= sample && x.end >= sample).ToList();
+            foreach (var note in adjacentNotes)
             {
-                note.hasInteracted = true;
+                if (hitableNotes.Contains(note))
+                {
+                    note.state = Note.NoteState.Hit;
+                    if (note.gameObject)
+                        note.gameObject.GetComponent<Image>().color = Color.green;
+                    Hit();
+                }
+                else
+                {
+                    note.state = Note.NoteState.Miss;
+                    if (note.gameObject)
+                        note.gameObject.GetComponent<Image>().color = Color.red;
+                    Miss();
+                }
             }
         }
         private void RemoveNotes(int sample)
         {
             foreach (Note.Direction dir in Enum.GetValues(typeof(Note.Direction)))
             {
-                var list = notes[dir].Where(x => x.hasInteracted || x.end < sample).ToList();
+                //var list = notes[dir].Where(x => x.state != Note.NoteState.Idle || x.end < sample).ToList();
+                var list = notes[dir].Where(x => x.end < sample).ToList();
                 if (list.Count > 0)
                 {
-                    notes[dir].RemoveWhere(x => x.hasInteracted || x.end < sample);
-                    item.SetActive(false);
+                    foreach (var n in list)
+                    {
+                        if (n.state == Note.NoteState.Idle)
+                            Miss();
+                        Destroy(n.gameObject);
+                        notes[dir].Remove(n);
+                    }
                 }
             }
+        }
+
+        private void Hit()
+        {
+            var node = notes[Note.Direction.Up].Where(x => x.end < 0).ToList()[0];
+            int middleSegment = node.start + (node.end - node.start) / 2;
+            print("hit!");
+        }
+
+        private void Miss()
+        {
+            print("miss!");
         }
     }
 }
